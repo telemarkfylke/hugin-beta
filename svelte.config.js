c2b41a8dc110131708362178c3e71856fdcb935c
SondreHaugom <sondrehaugom@gmail.com>
2025-12-01 08:25:12 +0100

    Oppdater adapter til node og rydd opp merge conflicts

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,8 +17,4 @@ const config = {
 	}
 }
 
-<<<<<<< HEAD
 export default config
-=======
-export default config
->>>>>>> main
