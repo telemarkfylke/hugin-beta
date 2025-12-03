805fcc5ec61a9efbd2b2243301e4dfef93cb807f
jorgtho <jorgen.thorsnes@gmail.com>
2025-12-03 11:47:39 +0100

    major: 1.0.0 - debug-authenticaton (#56)
    * feat: admin page for viewing user claims too see what they look like...
    
    * chore: bump version to 1.0.0

---
+++ src/lib/server/auth/auth-constants.ts
@@ -0,0 +1,4 @@
+export const MS_AUTH_PRINCIPAL_CLAIMS_HEADER = "x-ms-client-principal"
+export const MS_AUTH_PRINCIPAL_NAME_HEADER = "x-ms-client-principal-name"
+export const MS_AUTH_PRINCIPAL_ID_HEADER = "x-ms-client-principal-id"
+export const MS_AUTH_PRINCIPAL_IDP_HEADER = "x-ms-client-principal-idp"
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageProps } from "./$types"
+
+	// Get layout props, data will be accessible for children as well, so do not put too much here to avoid overfetching
+	let { data }: PageProps = $props()
+</script>
+  <div id="user-debug-for-now">
+	<h3>User claims for debugging (and actually knowing what they are...)</h3>
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+</div>
