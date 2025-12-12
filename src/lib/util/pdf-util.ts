aa02ff3eea3d0b11a165e6ccd86af31bce1c63b7
Rune Lunden <Teleruin@gmail.com>
2025-12-12 09:43:29 +0100

    Wrap tetfile content in a PDF to be able to upload it to open AI (#62)
    * convert remade
    
    * wrap text in pdf for openAi upload

---
+++ src/lib/util/pdf-util.ts
@@ -0,0 +1,28 @@
+import { PDFDocument, PDFImage, StandardFonts, rgb } from 'pdf-lib'
+export enum ImageType { JPG = 'image/jpeg', PNG = 'image/png' }
+
+export async function wrapImageInPdf(imageData: string, mimetype: ImageType): Promise<string> {
+	const pdfDoc = await PDFDocument.create()
+	let pdfImage: PDFImage;
+	if (mimetype === ImageType.JPG) {
+		pdfImage = await pdfDoc.embedJpg(imageData)
+	}
+	else if (mimetype === ImageType.PNG) {
+		pdfImage = await pdfDoc.embedPng(imageData)
+	}
+	else{
+		throw Error("Unsupported imageType") 
+	}
+	const page = pdfDoc.addPage([pdfImage.width, pdfImage.height])	
+	page.drawImage(pdfImage, { x: 0, y: 0 })	
+	const data = await pdfDoc.saveAsBase64()
+	return 'data:application/pdf;base64,'+data
+}
+
+export async function wrapTextInPdf(string: string): Promise<string> {
+	const pdfDoc = await PDFDocument.create()
+	const page = pdfDoc.addPage()
+	page.drawText(string)	
+	const data = await pdfDoc.saveAsBase64()
+	return 'data:application/pdf;base64,'+data
+}
