51948d3395657c7f3594f1e7950cf7f96af5b049
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-12-17 09:19:01 +0100

    fix: vite build need specific import to actually include the files...

--- src/lib/server/db/mockdb.ts
+++ src/lib/server/db/mockdb.ts
@@ -3,9 +3,9 @@ import type { DBAgent } from "$lib/types/agents.js"
 import type { DBConversation } from "$lib/types/conversation.js"
 
 export const getMockDb = async (): Promise<{ agents: DBAgent[]; conversations: DBConversation[] }> => {
-	// We add the mock agent here so it is always present in the mock db
-	const mockDbDataFile = env.TEST_MODE === "true" ? "./mockdb-test-data.js" : "./mockdb-data.js"
-	const { agents, conversations } = await import(mockDbDataFile)
+	const mockDbData = await import("./mockdb-data.js")
+	const mockTestDbData = await import("./mockdb-test-data.js")
+
 	console.log("./db/mockdb-data.js exists, loaded mockdb, returning mock collections")
-	return { agents, conversations }
+	return env.TEST_MODE === "true" ? mockTestDbData : mockDbData
 }
