edbff1a29c5c9bfd14ef6dc71afdc02a4d924049
Teleruin <Teleruin@gmail.com>
2025-12-29 15:28:12 +0100

    removed some leftovers

--- src/lib/types/agents.ts
+++ src/lib/types/agents.ts
@@ -97,7 +97,6 @@ export type IAgentResults = {
 		response: ReadableStream<Uint8Array>
 	}
 	AddConversationVectorStoreFilesResult: IVendorResults["AddVectorStoreFilesResult"]
-	AddVectorStoreFilesResult: IVendorResults["AddVectorStoreFilesResult"]
 	GetConversationVectorStoreFilesResult: IVendorResults["GetVectorStoreFilesResult"]
 	GetConversationVectorStoreFileContentResult: {
 		redirectUrl?: string
@@ -118,5 +117,4 @@ export interface IAgent {
 	getConversationVectorStoreFileContent(conversation: DBConversation, fileId: string): Promise<IAgentResults["GetConversationVectorStoreFileContentResult"]>
 	deleteConversationVectorStoreFile(conversation: DBConversation, fileId: string): Promise<void>
 	getConversationMessages(conversation: DBConversation): Promise<IAgentResults["GetConversationMessagesResult"]>
-	//	appendVectorStoreFiles(files: File[], streamResponse: boolean): Promise<IAgentResults["AddVectorStoreFilesResult"]>
 }
