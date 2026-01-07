7bc2a83cb48fb08894bcad72cbcd6727eae52148
Teleruin <Teleruin@gmail.com>
2026-01-07 15:44:39 +0100

    Added shell for mongoStores

--- src/lib/server/ioc/container.ts
+++ src/lib/server/ioc/container.ts
@@ -1,8 +1,10 @@
 import { env } from "$env/dynamic/private"
 import type { IAgentStore } from "../db/agents/interface.js"
 import { MockAgentStore } from "../db/agents/mock_agentstore.js"
+import { MongoAgentStore } from "../db/agents/mongo_agentstore.js"
 import type { IConversationStore } from "../db/conversations/interface"
 import { MockConversationStore } from "../db/conversations/mock_conversationstore"
+import { MongoConversationStore } from "../db/conversations/mongo_conversationstore.js"
 import { agents as mockAgents, conversations as mockConversations } from "../db/mockdb-data.js"
 import { agents as testAgents, conversations as testConversations } from "../db/mockdb-test-data.js"
 import type { IVectorStoreDb } from "../db/vectorstore/interface"
@@ -26,8 +28,8 @@ export function getIocContainer(): IocContainer {
 		iocContainer = {
 			vectorStore: env.MOCK_VECTORSTORE === "true" ? new MockVectorStoreDb() : new MongoVectorStoreDb(),
 			embedder: env.MOCK_EMBEDDER === "true" ? new MockEmbedder() : new OllamaEmbedder(),
-			conversationStore: new MockConversationStore(env.TEST_MODE === "true" ? testConversations : mockConversations),
-			agentStore: new MockAgentStore(env.TEST_MODE === "true" ? testAgents : mockAgents)
+			conversationStore: env.MOCK_DB === "true" ? new MockConversationStore(env.TEST_MODE === "true" ? testConversations : mockConversations) : new MongoConversationStore(),
+			agentStore: env.MOCK_DB === "true" ? new MockAgentStore(env.TEST_MODE === "true" ? testAgents : mockAgents) : new MongoAgentStore()
 		}
 	}
 
