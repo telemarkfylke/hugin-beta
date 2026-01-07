d68582f25ffa05e90c3f400e9dfbd438f85eca3e
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-01-07 13:11:46 +0100

    feat: typing animation for fun

--- src/lib/components/Chat/ChatMessage.svelte
+++ src/lib/components/Chat/ChatMessage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { ChatInputMessage, ChatResponseObject } from "$lib/types/chat"
+    import TypingDots from "../TypingDots.svelte";
 	import ChatMessageItem from "./ChatMessageItem.svelte"
 
 	type Props = {
@@ -16,11 +17,16 @@
   <div class="chat-response">
     <h4>{chatConfigName} ({chatMessage.status})</h4>
     {#if chatMessage.status === "queued" || chatMessage.status === "in_progress" && chatMessage.outputs.length === 0}
-      jobber med det...
+      <TypingDots />
     {:else}
       {#each chatMessage.outputs as chatItem}
         <ChatMessageItem {chatItem} />
       {/each}
     {/if}
+    <!--
+    <div class="chat-response-usage">
+      Usage: {chatMessage.usage ? `Prompt tokens: ${chatMessage.usage.inputTokens}, Completion tokens: ${chatMessage.usage.outputTokens}, Total tokens: ${chatMessage.usage.totalTokens}` : 'N/A'}
+    </div>
+    -->
   </div>
 {/if}
