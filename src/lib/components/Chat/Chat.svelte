ca9837e1ae3479fbbc83147b7fb2c4179ca34991
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-01-08 23:24:06 +0100

    feat: frekk og freidig (wip scroll)

--- src/lib/components/Chat/Chat.svelte
+++ src/lib/components/Chat/Chat.svelte
@@ -2,44 +2,79 @@
 	import ChatInput from "./ChatInput.svelte"
 	import ChatHistoryItem from "./ChatHistoryItem.svelte"
   import type { ChatState } from "./ChatState.svelte";
+    import { tick } from "svelte";
 
 	type Props = {
 		chatState: ChatState
 	}
 
 	let { chatState }: Props = $props()
+
+	let showConfig = $state(false)
+
+	let lastChatItem: HTMLDivElement
+
+	$effect(() => {
+		const lastChat = chatState.chat.history[chatState.chat.history.length - 1]
+		if (lastChat?.type === 'chat_response') {
+			const lastItem = lastChat.outputs[lastChat.outputs.length - 1]
+			if (lastItem?.type === 'message.output') {
+				const lastContent = lastItem.content[lastItem.content.length - 1]
+				if (lastContent?.type === 'output_text') {
+					lastContent.text
+					$inspect(lastContent.text)
+				} else if (lastContent?.type === 'output_refusal') {
+					lastContent.reason
+					$inspect(lastContent.reason)
+				}
+			}
+		}
+		tick().then(() => {
+			lastChatItem.scrollIntoView({ behavior: "smooth" })
+		})
+	})
 	
 </script>
 
 <div class="chat-container">
-	<div class="chat-config">
-		<h3>{chatState.chat.config.name}</h3>
-		{JSON.stringify(chatState.chat.config, null, 2)}
-		<br />
-		<select bind:value={chatState.chat.config.vendorId}>
-			<option value="openai">OpenAI</option>
-		</select>
-		<br />
-		{#if chatState.chat.config.vendorAgent}
-			<p>Agent-id: {chatState.chat.config.vendorAgent.id}</p>
-		{:else}
-			<span>Model:</span>
-			<select bind:value={chatState.chat.config.model}>
-				<option value="gpt-4o">GPT-4o</option>
-				<option value="gpt-4">GPT-4</option>
+	<button onclick={() => { lastChatItem.scrollIntoView({ behavior: "smooth" }) }}>LScroll</button>
+	<div class="chat-header">
+		<h2>Chat ID: {chatState.chat.config.name}</h2>
+		<button onclick={() => showConfig = !showConfig}>
+			{#if showConfig} Hide Config {:else} Show Config {/if}
+		</button>
+	</div>
+	{#if showConfig}
+		<div class="chat-config">
+			{JSON.stringify(chatState.chat.config, null, 2)}
+			<br />
+			<span>Vendor:</span>
+			<select bind:value={chatState.chat.config.vendorId}>
+				<option value="openai">OpenAI</option>
 			</select>
 			<br />
-			<span>instructions:</span>
-			<input type="text" bind:value={chatState.chat.config.instructions} />
-			<br />
-			<input type="checkbox" bind:checked={chatState.streamResponse} /> Stream
-			<br />
-		{/if}
-	</div>
-	<div class="chat-items">
+			{#if chatState.chat.config.vendorAgent}
+				<p>Agent-id: {chatState.chat.config.vendorAgent.id}</p>
+			{:else}
+				<span>Model:</span>
+				<select bind:value={chatState.chat.config.model}>
+					<option value="gpt-4o">GPT-4o</option>
+					<option value="gpt-4">GPT-4</option>
+				</select>
+				<br />
+				<span>instructions:</span>
+				<input type="text" bind:value={chatState.chat.config.instructions} />
+				<br />
+				<input type="checkbox" bind:checked={chatState.streamResponse} /> Stream
+				<br />
+			{/if}
+		</div>
+	{/if}
+	<div class="chat-items" bind:this={chatHistoryDiv}>
 		{#each chatState.chat.history as chatHistoryItem}
 			<ChatHistoryItem {chatHistoryItem} />
 		{/each}
+		<div bind:this={lastChatItem}>sist</div>
 	</div>
 
 	<ChatInput allowedFileMimeTypes={[]} sendMessage={chatState.promptChat} />
@@ -54,6 +89,14 @@
     margin: 0 auto;
     height: 100%;
   }
+	.chat-header {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+	.chat-config {
+		border-bottom: 1px solid #ccc;
+	}
 	.chat-items {
     flex: 1;
     padding: 0.3rem;
