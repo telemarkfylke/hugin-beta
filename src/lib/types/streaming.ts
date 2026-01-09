131e2b6a16eb4d87a218cf1f676f2a6d3ce48ad4
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-01-09 13:15:08 +0100

    fix: removed unused events

--- src/lib/types/streaming.ts
+++ src/lib/types/streaming.ts
@@ -76,74 +76,6 @@ const UnknownEvent = z.object({
 	data: z.unknown()
 })
 
-// Old and shitty?
-const ConversationStarted = z.object({
-	event: z.literal("conversation.started"),
-	data: z.object({
-		conversationId: z.string()
-	})
-})
-
-const ConversationMessageDelta = z.object({
-	event: z.literal("conversation.message.delta"),
-	data: z.object({
-		messageId: z.string(),
-		content: z.string()
-	})
-})
-
-const ConversationMessageEnded = z.object({
-	event: z.literal("conversation.message.ended"),
-	data: z.object({
-		totalTokens: z.number()
-	})
-})
-
-const ConversationEnded = z.object({
-	event: z.literal("conversation.ended"),
-	data: z.object({
-		conversationId: z.string()
-	})
-})
-
-const ErrorEvent = z.object({
-	event: z.literal("error"),
-	data: z.object({
-		message: z.string()
-	})
-})
-
-const ConversationVectorStoreCreated = z.object({
-	event: z.literal("conversation.vectorstore.created"),
-	data: z.object({
-		vectorStoreId: z.string()
-	})
-})
-
-const VendorFileProcessed = z.object({
-	event: z.literal("vendor.vectorstore.file.processed"),
-	data: z.object({
-		fileId: z.string(),
-		fileName: z.string()
-	})
-})
-
-const ConversationFileUploaded = z.object({
-	event: z.literal("conversation.vectorstore.file.uploaded"),
-	data: z.object({
-		fileId: z.string(),
-		fileName: z.string()
-	})
-})
-
-const ConversationFilesProcessed = z.object({
-	event: z.literal("conversation.vectorstore.files.processed"),
-	data: z.object({
-		files: z.array(z.object({ fileId: z.string() })),
-		vectorStoreId: z.string()
-	})
-})
-
 export const MuginSse = z.discriminatedUnion("event", [
 	// New events
 	ResponseConfig,
@@ -152,17 +84,7 @@ export const MuginSse = z.discriminatedUnion("event", [
 	ResponseError,
 	ResponseOutputTextDelta,
 	ConversationCreated,
-	UnknownEvent,
-	// Old events
-	ConversationStarted,
-	ConversationMessageDelta,
-	ConversationMessageEnded,
-	ConversationEnded,
-	ErrorEvent,
-	ConversationVectorStoreCreated,
-	ConversationFileUploaded,
-	ConversationFilesProcessed,
-	VendorFileProcessed
+	UnknownEvent
 ])
 
 export type MuginSse = z.infer<typeof MuginSse>
