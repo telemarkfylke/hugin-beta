87f01af3a12e719c81834b362a95ae04ddb88a56
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-01-09 21:58:55 +0100

    fix: types instead of zod in auth

--- src/lib/types/authentication.ts
+++ src/lib/types/authentication.ts
@@ -5,106 +5,21 @@ import z from "zod"
  * https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-user-identities
  * https://learn.microsoft.com/en-us/entra/identity-platform/id-token-claims-reference#payload-claims
  */
-export const MSUserClaim = z.discriminatedUnion("typ", [
-	z.object({
-		typ: z.literal("aud"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("iss"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("iat"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("nbf"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("exp"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("aio"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("c_hash"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("groups"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("name"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("nonce"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("http://schemas.microsoft.com/identity/claims/objectidentifier"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("preferred_username"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("rh"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("sid"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("http://schemas.microsoft.com/identity/claims/tenantid"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("uti"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("ver"),
-		val: z.string()
-	}),
-	z.object({
-		typ: z.literal("roles"),
-		val: z.string()
-	})
-])
+export type MSPrincipalClaimTyp = "aud" | "iss" | "iat" | "nbf" | "exp" | "aio" | "c_hash" | "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress" | "groups" | "name" | "nonce" | "http://schemas.microsoft.com/identity/claims/objectidentifier" | "preferred_username" | "rh" | "sid" | "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier" | "http://schemas.microsoft.com/identity/claims/tenantid" | "uti" | "ver" | "roles" | "cc"
 
-export const MSUserClaimOLD = z.object({
-	typ: z.string(),
-	val: z.string()
-})
+export type MSPrincipalClaim = {
+	typ: MSPrincipalClaimTyp,
+	val: string
+}
 
-export type MSUserClaim = z.infer<typeof MSUserClaim>
+export type MSPrincipalClaims = {
+	auth_typ: string,
+	claims: MSPrincipalClaim[],
+	name_typ: string,
+	role_typ: string
+}
 
-export const MSPrincipalClaims = z.object({
-	auth_typ: z.string(),
-	claims: z.array(MSUserClaim),
-	name_typ: z.string(),
-	role_typ: z.string()
-})
-
-export type MSPrincipalClaims = z.infer<typeof MSPrincipalClaims>
-
-export const AuthenticatedUser = z.object({
+export const AuthenticatedPrincipalSchema = z.object({
 	/** ObjectId in EntraID */
 	userId: z.string(),
 	name: z.string(),
@@ -116,4 +31,4 @@ export const AuthenticatedUser = z.object({
 	groups: z.array(z.string())
 })
 
-export type AuthenticatedUser = z.infer<typeof AuthenticatedUser>
+export type AuthenticatedPrincipal = z.infer<typeof AuthenticatedPrincipalSchema>
