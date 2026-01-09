a48827392235dd67b4b13318c288c9fec5b1bf55
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-01-09 23:05:57 +0100

    fix: create auto growing text area component

--- src/lib/components/Chat/ChatInput.svelte
+++ src/lib/components/Chat/ChatInput.svelte
@@ -2,6 +2,7 @@
 	import type { ChatConfig } from "$lib/types/chat"
 	import { VENDOR_SUPPORTED_MESSAGE_MIME_TYPES } from "$lib/vendor-constants"
 	import FileDropZone from "../FileDropZone.svelte"
+    import GrowingTextArea from "../GrowingTextArea.svelte";
 
 	type Props = {
 		chatConfig: ChatConfig
@@ -23,7 +24,7 @@
 	let inputFiles = $state(new DataTransfer().files)
 
 	// Simple helper for posting prompt, and clearing input
-	const submitPrompt = async (): Promise<void> => {
+	const submitPrompt = (): void => {
 		if (inputText.trim() === "" && inputFiles.length === 0) {
 			return // Do not submit empty prompts
 		}
@@ -31,21 +32,9 @@
 		const filesToSend = inputFiles
 		inputFiles = new DataTransfer().files // Clear chat files after submission
 		inputText = ""
-		await sendMessage(textToSend, filesToSend)
+		sendMessage(textToSend, filesToSend)
 	}
 
-	// Some element references
-	let textArea: HTMLTextAreaElement
-	let wrapDiv: HTMLDivElement
-	/**
-	 * As we wait for "textarea {field-sizing: content;}" to be supported in all browsers
-	 * Magic is in CSS below, this JS just updates the data attribute on input
-	 * Thank you to Chris Coyier and Stephen Shaw
-	 * @link https://chriscoyier.net/2023/09/29/css-solves-auto-expanding-textareas-probably-eventually/
-	 */
-	const sneakyTextArea = () => {
-		wrapDiv.setAttribute("data-replicated-value", textArea.value)
-	}
 	const submitOnEnter = (event: KeyboardEvent) => {
 		if (event.key === "Enter" && !event.shiftKey) {
 			event.preventDefault()
@@ -57,9 +46,7 @@
 <div>
 	<FileDropZone onFilesDropped={(files) => { inputFiles = files; }} />
   <form onsubmit={(event: Event) => { event.preventDefault(); submitPrompt() }}>
-    <div class="grow-wrap" bind:this={wrapDiv}>
-      <textarea rows="1" bind:this={textArea} placeholder="Skriv et eller annet (shift + enter for ny linje)" name="prompt-input" id="prompt-input" oninput={sneakyTextArea} onkeydown={submitOnEnter} bind:value={inputText}></textarea>
-    </div>
+    <GrowingTextArea bind:inputText={inputText} placeholder="Type your message here..." onkeydown={submitOnEnter} />
     <div id="actions">
       <div id="actions-left">
         <div id="chat-file-upload-container">
@@ -87,41 +74,4 @@
     display: flex;
     justify-content: space-between;
   }
-
-  /* START AUTO GROW TEXTAREA STYLES */
-  .grow-wrap::after, #prompt-input {
-    max-height: 8rem;
-  }
-  .grow-wrap {
-    /* easy way to plop the elements on top of each other and have them both sized based on the tallest one's height */
-    display: grid;
-  }
-  .grow-wrap::after {
-    /* Note the weird space! Needed to preventy jumpy behavior */
-    content: attr(data-replicated-value) " ";
-
-    /* This is how textarea text behaves */
-    white-space: pre-wrap;
-
-    /* Hidden from view, clicks, and screen readers */
-    visibility: hidden;
-  }
-  .grow-wrap > textarea {
-    /* You could leave this, but after a user resizes, then it ruins the auto sizing */
-    resize: none;
-
-    /* Firefox shows scrollbar on growth, you can hide like this. */
-    /* overflow: hidden; */
-  }
-  .grow-wrap > textarea,
-  .grow-wrap::after {
-    /* Identical styling required!! */
-    border: 1px solid black;
-    padding: 0.5rem;
-    font: inherit;
-
-    /* Place on top of each other */
-    grid-area: 1 / 1 / 2 / 2;
-  }
-  /* END AUTO GROW TEXTAREA STYLES */
 </style>
