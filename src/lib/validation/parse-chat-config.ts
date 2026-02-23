a97b072eee1fa8474c1008abc628f4ab8d3e9b96
Teleruin <Teleruin@gmail.com>
2026-02-23 11:31:16 +0100

    Rewrite comment

--- src/lib/validation/parse-chat-config.ts
+++ src/lib/validation/parse-chat-config.ts
@@ -42,6 +42,8 @@ export const parseChatConfig = (input: unknown, APP_CONFIG: AppConfig): ChatConf
 	if (!VENDOR.MODELS.some((model) => model.ID === parsedConfig.model)) {
 		throw new HTTPError(400, `Unsupported model: ${parsedConfig.model} for vendorId: ${parsedConfig.vendorId}`)
 	}
+
+	// NB : Husk å legge til propertiene i dette objektet også. Spessielst hvir det er optional så er det lett å glemme. 
 	return {
 		_id: parsedConfig._id,
 		name: parsedConfig.name,
@@ -51,7 +53,7 @@ export const parseChatConfig = (input: unknown, APP_CONFIG: AppConfig): ChatConf
 		model: parsedConfig.model,
 		instructions: parsedConfig.instructions,
 		conversationId: parsedConfig.conversationId,
-		tools: parsedConfig.tools || [], // <- Jeg vil bare si at jeg brukte 2 timer på å finne ut av at denne måtte være her. Hilsen, fuzzbin.  La til en default fra nul ltil tom array her jeg.
+		tools: parsedConfig.tools || [],
 		shared: parsedConfig.shared,
 		accessGroups: parsedConfig.accessGroups,
 		type: parsedConfig.type,
