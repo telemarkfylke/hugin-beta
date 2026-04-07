3ed20278a70112c7be29e810f5610a6ba5040c4f
Teleruin <Teleruin@gmail.com>
2026-04-07 12:50:30 +0200

    lint

--- src/lib/server/transcription/transcription.ts
+++ src/lib/server/transcription/transcription.ts
@@ -4,7 +4,13 @@ import { env } from "$env/dynamic/private"
 
 type TranscriptionMetadata = { filnavn: string; spraak: string; format: string; selectedFileName: string | null }
 
-const { VITE_AI_API_URI: aiApiUri, VITE_TRANSCRIPTION_APPREG_ID: client_id, VITE_TRANSCRIPTION_APPREG_KEY: client_secret, VITE_APPREG_TENANT_ID: tenant_id, VITE_TRANSCRIPTION_SCOPES: scopes, } = import.meta.env
+const {
+	VITE_AI_API_URI: aiApiUri,
+	VITE_TRANSCRIPTION_APPREG_ID: client_id,
+	VITE_TRANSCRIPTION_APPREG_KEY: client_secret,
+	VITE_APPREG_TENANT_ID: tenant_id,
+	VITE_TRANSCRIPTION_SCOPES: scopes
+} = import.meta.env
 const authority = `https://login.microsoftonline.com/${tenant_id}`
 
 const config = {
@@ -23,18 +29,18 @@ const tokenRequest = {
 	scopes: scopes.split(";") // or your API scope
 }
 
-
-async function sendWithFetch(datapakken: FormData, token: string): Promise<Response> {
+/*
+async function _sendWithFetch(datapakken: FormData, token: string): Promise<Response> {
 	return fetch(`${aiApiUri}/nbTranscript`, {
-		method: 'POST',
+		method: "POST",
 		headers: {
-			'Content-Type': 'multipart/form-data',
+			"Content-Type": "multipart/form-data",
 			authorization: `Bearer ${token}`
 		},
 		body: datapakken
 	})
 }
-
+*/
 async function sendWithAxios(datapakken: FormData, token: string): Promise<Response> {
 	return axios.post(`${aiApiUri}/nbTranscript`, datapakken, {
 		method: "post",
@@ -45,8 +51,6 @@ async function sendWithAxios(datapakken: FormData, token: string): Promise<Respo
 		}
 	})
 }
-
-
 
 export async function sendFileToTranscription(userUpn: string, filliste: Blob, metadata: TranscriptionMetadata): Promise<{ responseCode: number; message: string }> {
 	/*
@@ -64,8 +68,7 @@ export async function sendFileToTranscription(userUpn: string, filliste: Blob, m
 	const token = env.TRANSCRIPTION_MOCK_TOKEN ? env.TRANSCRIPTION_MOCK_TOKEN : await getToken()
 
 	try {
-
-		//const response = await sendWithFetch(datapakken, token)
+		//const response = await _sendWithFetch(datapakken, token)
 		const response = await sendWithAxios(datapakken, token)
 
 		const resMessage = response.statusText
