d268d1cf2a7ff8689029f140574507882fed4eda
Teleruin <Teleruin@gmail.com>
2026-04-20 10:42:42 +0200

    Remove old conversationexport diabeled

--- src/lib/types/app-config.ts
+++ src/lib/types/app-config.ts
@@ -25,7 +25,6 @@ export type AppConfig = {
 	NAME: string
 	BODY_SIZE_LIMIT_BYTES: number
 	APP_ROLES: AppRoles
-	CONVERSATION_EXPORT_DISABLED: boolean
 	NEW_CHAT_CONFIRM_DISABLED: boolean
 	VENDORS: {
 		MISTRAL: VendorInfo
