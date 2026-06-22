697dac51ed0824844f40840426623f6a4ade8c1f
Teleruin <Teleruin@gmail.com>
2026-06-23 01:20:15 +0200

    ragservice

---
+++ src/lib/ragservice/components/DataStoreCreate.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+	import { RagServiceApi } from "../adapters/ragserviceApi";
+	import { afterNavigate } from '$app/navigation';
+	import type {
+		CreateVectorStoreInput,
+		EmbeddingDimensions,
+		EmbeddingModel,
+	} from "../types";
+
+	type Props = {
+		onDone: (storeId: string | null) => void
+	};
+
+	
+	let { onDone }: Props = $props();
+
+	let methodOptions: EmbeddingModel[] = $state(["embeddinggemma:300m"]);
+	let dimensionsOptions: EmbeddingDimensions[] = $state([]);
+
+	const store: CreateVectorStoreInput = $state({
+		name: "",
+		description: "",
+		embeddingMethod: "embeddinggemma:300m",
+		dimensions: 1024,
+	});
+
+	const api = new RagServiceApi();
+
+	async function loadModels() {
+		methodOptions = await api.getModels();
+		await loadDimensions();
+	}
+
+	async function loadDimensions() {
+		dimensionsOptions = await api.getDimensions(store.embeddingMethod);
+		if (!dimensionsOptions.includes(store.dimensions)) {
+			if (dimensionsOptions.length > 0) {
+				store.dimensions = dimensionsOptions[0]!;
+			}
+		}
+	}
+
+	async function addStore() {
+		const reply = await api.createStore(store);
+		onDone(reply?.storeId || null)
+	}
+
+	async function cancel() {
+		onDone(null)
+	}
+
+	afterNavigate(() => {
+		loadModels();
+	});
+</script>
+
+<main>
+	<table>
+		<tbody>
+			<tr><td>Navn</td><td>:</td><td><input bind:value={store.name} /></td></tr>
+			<tr
+				><td>Beskrivelse</td><td>:</td><td
+					><input bind:value={store.description} /></td
+				></tr
+			>
+			<tr
+				><td>Method</td><td>:</td><td>
+					<select
+						bind:value={store.embeddingMethod}
+						onchange={() => {
+							loadDimensions();
+						}}
+					>
+						{#each methodOptions as option}
+							<option value={option}>{option}</option>
+						{/each}
+					</select></td
+				></tr
+			>
+			<tr
+				><td>Dimensions</td><td>:</td><td>
+					<select bind:value={store.dimensions}>
+						{#each dimensionsOptions as option}
+							<option value={option}>{option}</option>
+						{/each}
+					</select></td
+				></tr
+			>
+		</tbody>
+	</table>
+	<button onclick={() => {cancel()}}>Avbryt</button>
+	<button onclick={() => {addStore()}}>Legg til</button>
+</main>
